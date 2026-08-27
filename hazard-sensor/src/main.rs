06e15f8dfcd6ad105768c2800dbc2771392f4228
g-emer <u6892552@anu.edu.au>
2026-08-27 18:31:25 +1000

    rough shapes for soil moisture and wind speed done

--- hazard-sensor/src/main.rs
+++ hazard-sensor/src/main.rs
@@ -2,22 +2,38 @@
 #![no_main]
 
 use {
-    crate::sensors::tipping_bucket::TippingBucket, core::error::Error, defmt_rtt as _,
-    panic_probe as _,
+    crate::sensors::soil_moisture::SoilMoisture, crate::sensors::tipping_bucket::TippingBucket,
+    core::error::Error, defmt_rtt as _, panic_probe as _,
 };
 
 mod sensors;
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_nrf::*;
+use embassy_nrf::{
+    saadc::{ChannelConfig, Config, Saadc},
+    *,
+};
+use sensors::soil_moisture;
 use sensors::tipping_bucket;
 use static_cell::ConstStaticCell;
 
 bind_interrupts!(struct Irqs {
     TWISPI0 => twim::InterruptHandler<peripherals::TWISPI0>;
     UARTE1  => uarte::InterruptHandler<peripherals::UARTE1>;
+    SAADC => saadc::InterruptHandler;
 });
+
+fn init_adc(
+    saadc: Peri<'static, peripherals::SAADC>,
+    soil_moisture_pin: Peri<'static, peripherals::P0_02>, // confirm actual RAK4631 mapping
+    wind_speed_pin: Peri<'static, peripherals::P0_03>,    // confirm actual RAK4631 mapping
+) -> Saadc<'static, 2> {
+    let config = Config::default();
+    let channel0 = ChannelConfig::single_ended(soil_moisture_pin);
+    let channel1 = ChannelConfig::single_ended(wind_speed_pin);
+    Saadc::new(saadc, Irqs, config, [channel0, channel1])
+}
 
 static TX_BUFF: ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
 
--- hazard-sensor/src/sensors/mod.rs
+++ hazard-sensor/src/sensors/mod.rs
@@ -1,2 +1,4 @@
+pub mod soil_moisture;
 pub mod tipping_bucket;
 pub mod wind_sensor;
+pub mod wind_speed;

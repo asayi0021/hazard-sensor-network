3a4a7f02063792e25200638fc94bd76056ef4387
g-emer <u6892552@anu.edu.au>
2026-08-27 17:15:49 +1000

    Structure has been updated

--- hazard-sensor/src/sensors/tipping_bucket.rs
+++ hazard-sensor/src/sensors/tipping_bucket.rs
@@ -1,34 +1,60 @@
-use crate::Irqs;
+use crate::{Irqs, sensors::tipping_bucket::InputRegister::TimeRainfallLReg};
 use async_modbus::client::{read_inputs, write_holding};
-use defmt::debug;
+use defmt::{Format, debug};
 use embassy_nrf::{
     Peri, peripherals,
     uarte::{self, Baudrate, Config, Parity, Uarte},
 };
-
-// Make a enum for registers
-const RAIN_HOUR_HOLDING_REG: u16 = 0x0006;
-const TIME_RAINFALL_L_REG: u16 = 0x0006;
 
 //use nrf_pac::{radio::vals::State::Rx, wdt::regs::Config};
 //use nrf_pac::uarte::regs::Baudrate;
 
 // SEN0575 Tipping Bucket Rainfall Sensor
 pub struct TippingBucket {
-    //
-    parity: uarte::Parity,
-    //
-    baudrate: uarte::Baudrate,
+    //     //
+    //     parity: uarte::Parity,
+    //     //
+    //     baudrate: uarte::Baudrate,
+    //     //
 }
+
+#[derive(Format, Clone)]
+//The names of these registers mirror the given python library's input register names for the tipping bucket sensor
+pub enum InputRegister {
+    InputRegPID = 0x0000,
+    InputRegVID = 0x0001,
+    InputRegBaud = 0x0003,
+    InputRegVerifyAndStop = 0x0004,
+    TimeRainfallLReg = 0x0006,
+}
+
+#[derive(Format, Clone)]
+//The names of these registers mirror the given python library's holding register names for the tipping bucket sensor
+pub enum HoldingRegister {
+    RainHourHoldingReg = 0x0006,
+}
+
+// Hardcoded values found in the given python library
+pub const SLAVE_ADDR: u8 = 0xC0;
+pub const READ_REGISTER_COUNT: u16 = 2;
+pub const EXPECTED_PID: u32 = 0x100C0;
+pub const EXPECTED_VID: u32 = 0x3343;
 
 pub enum SensorError {
     GetDataError,
     UARTError(uarte::Error),
+    UnexpectedDevice,
 }
 
 impl From<uarte::Error> for SensorError {
     fn from(value: uarte::Error) -> Self {
         SensorError::UARTError(value)
+    }
+}
+
+impl From<read_inputs> for SensorError {
+    fn from(value: uarte::Error) -> Self {
+        SensorError(value)
     }
 }
 
@@ -40,8 +66,8 @@ impl TippingBucket {
         tx_pin: Peri<'static, peripherals::P0_10>, //Double check that this pin is correct
     ) -> Result<Uarte<'static>, SensorError> {
         let mut config = Config::default();
-        config.parity = Parity::Included; //Double check later what parity is needed
-        config.baudrate = Baudrate::Baud115200; //Double check later what baudrate is needed
+        config.parity = Parity::Excluded; //Based on what found in the given library for the sensor
+        config.baudrate = Baudrate::Baud9600; //Based on what found in the given library for the sensor
         let uart = Uarte::new(uarte1, rx_pin, tx_pin, Irqs, config);
         debug!("tipping bucket initialised succesfully");
         Ok(uart)
@@ -51,7 +77,8 @@ impl TippingBucket {
     pub async fn init_tipping_bucket_modbus(
         uart: &mut Uarte<'static>,
     ) -> Result<(u32, u32), SensorError> {
-        let regs = read_inputs::<2, _>(&mut *uart, SLAVE_ADDR, PID_VID_REG).await?;
+        let regs =
+            read_inputs::<2, _>(&mut *uart, SLAVE_ADDR, InputRegiser::InputRegPID as u16).await?;
 
         let reg0 = regs[0].get() as u32; // PID low word
         let reg1 = regs[1].get() as u32; // VID + PID high bits, packed
@@ -67,8 +94,8 @@ impl TippingBucket {
         rx_pin: Peri<'static, peripherals::P0_09>, //Double check that this pin is correct
         tx_pin: Peri<'static, peripherals::P0_10>, //Double check that this pin is correct
     ) -> Result<Uarte<'static>, SensorError> {
-        let mut uart = init_tipping_bucket_uarte(uarte1, rx_pin, tx_pin)?;
-        let (pid, vid) = init_tipping_bucket_modbus(&mut uart).await?;
+        let mut uart = Self::init_tipping_bucket_uarte(uarte1, rx_pin, tx_pin)?;
+        let (pid, vid) = Self::init_tipping_bucket_modbus(&mut uart).await?;
 
         if pid != EXPECTED_PID || vid != EXPECTED_VID {
             //find the expected values
@@ -79,14 +106,25 @@ impl TippingBucket {
     }
 
     // Gets the tipping bucket value from the sensor - specifially from the set time cumulative rainfall registers
+    // Potentially add a flag for div by 10000.0 But need to decide later.
     pub async fn get_tipping_bucket(
         uart: &mut Uarte<'static>,
-        slave_addr: u8,
         hours: u8,
     ) -> Result<f32, SensorError> {
-        write_holding(&mut *uart, slave_addr, RAIN_HOUR_HOLDING_REG, hours as u16).await?;
+        write_holding(
+            &mut *uart,
+            SLAVE_ADDR,
+            RainHourHoldingReg as u16,
+            hours as u16,
+        )
+        .await?;
 
-        let regs = read_inputs::<2, _>(&mut *uart, slave_addr, TIME_RAINFALL_L_REG).await?;
+        let regs = read_inputs::<2, _>(
+            &mut *uart,
+            SLAVE_ADDR,
+            InputRegister::TimeRainfallLReg as u16,
+        )
+        .await?;
         let raw = (regs[1].get() as u32) << 16 | (regs[0].get() as u32);
         let rainfall_mm = raw as f32 / 10000.0;
 
